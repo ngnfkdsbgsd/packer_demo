31f04361523dcb9860f4ff911734676ce9221422
never <never.foobar@gmail.com>
2022-05-04 03:09:30 +0300

    typo

--- provision/install_openssh.ps1
+++ provision/install_openssh.ps1
@@ -3,9 +3,8 @@ Start-Process msiexec -ArgumentList "/i OpenSSH-x64-v8.9.1.0.msi /quiet" -Wait
 Get-Service "ssh*" | Stop-Service -Force
 
 $env_path = [System.Environment]::GetEnvironmentVariable("path")
-$path = ";$($env:SystemDrive)\Program Files(x86)\OpenSSH"
-if ([System.Environment]::Is64BitOperatingSystem) { $path += ";$($env:SystemDrive)\Program Files\OpenSSH" }
-[System.Environment]::SetEnvironmentVariable("path", $($env_path.Insert($env_path.Length, ";$($env:SystemDrive)\Program Files\OpenSSH;%systemdrive%\Program Files(x86)\OpenSSH")), "Machine")
+$path = ";$($env:SystemDrive)\Program Files\OpenSSH"
+[System.Environment]::SetEnvironmentVariable("path", $($env_path.Insert($env_path.Length, $path)), "Machine")
 
 New-ItemProperty -Path "HKLM:\SOFTWARE\OpenSSH" -Name "DefaultShell" -Value "$($env:SystemDrive)\Windows\System32\WindowsPowerShell\v1.0\powershell.exe" | Out-Null	
 Copy-Item -Path ".\sshd_config_wim" -Destination ([System.IO.Path]::Combine($env:ProgramData, "ssh", "sshd_config")) -Force
--- provision/install_ps51.ps1
+++ provision/install_ps51.ps1
@@ -1,5 +1,5 @@
 $file = [System.IO.Path]::Combine($env:Temp, "Win8.1AndW2K12R2-KB3191564-x64.msu")
 $log1 = [System.IO.Path]::Combine($env:Temp, "ps51_install.log")
 $log2 = [System.IO.Path]::Combine($env:Temp, "ps51_install_err.log")
-$proc = Start-Process wusa.exe -ArgumentList "$file /quiet /norestart" -Wait -PassThru 
+$proc = Start-Process wusa.exe -ArgumentList "$file /quiet /norestart" -Wait -PassThru -RedirectStandardError $log2 -RedirectStandardOutput $log1
 return $proc.ExitCode
